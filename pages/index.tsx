d5b3a930aa3d2cba49209b586877392cbb0a818a
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-04 13:04:44 +0530

    convert js -> ts and remove unrequired files

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Task Manager</title>
+        <meta name="description" content="Simple Task Manager in ReactJS" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
